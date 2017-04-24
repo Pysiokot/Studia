dbb8036189da5916aa47eb979eff50e5a5e5d65c
Pysiokot <szcz.bart@gmail.com>
2017-04-24 15:54:24 +0200

    Basic pollution module works perfectly

--- Erlang/Lab2/pollution.erl
+++ Erlang/Lab2/pollution.erl
@@ -3,19 +3,29 @@
 
 -export([createMonitor/0]).
 -export([addStation/3]).
--export([addValue/5,removeValue/4,getOneValue/4,getStationMean/3,getDailyMean/3]).
+-export([addValue/5]).
+-export([removeValue/4]).
+-export([getOneValue/4]).
+-export([getStationMean/3]).
+-export([getDailyMean/3]).
 
--record(coordinates, {vertical, horizontal}).
+-record(coordinates, {horizontal, vertical}).
 -record(measurement, {type, value, date}).
 
+%działa
 createMonitor() -> #{}.
 
-addStation(Name, Coords, Monitor) ->
-    case maps:find(Name) of
-        {ok, _}     -> Monitor;
-        _           -> Monitor#{Name => {Coords, []}}
+%działa
+addStation(Name, {X, Y}, Monitor) ->
+    case maps:find(Name, Monitor) of
+        {ok, _}     -> throw("Station with this name exists");
+        _           -> case findCoords({X,Y}, maps:values(Monitor)) of
+            false   -> maps:put(Name, {#coordinates{horizontal=X,vertical=Y}, []}, Monitor);
+            true    -> throw("Station with these coordinates exists")
+        end
     end.
 
+%działa
 addValue(Station, Date, Type, Value, Monitor) ->
     case maps:is_key(Station, Monitor) of
         true -> 
@@ -24,80 +34,103 @@ addValue(Station, Date, Type, Value, Monitor) ->
             maps:remove(Station, Monitor),
             Monitor#{Name => addMeasurement(Value, Date, Type, Val)};
         false -> 
-            case findCoords(maps:values(Monitor), Station) of
+            case findCoords(Station, maps:values(Monitor)) of
             true -> 
-                Val = maps:get(Station, Monitor),
-                Monitor2 = maps:remove(Station, Monitor),
-                Monitor2#{Station => addMeasurement(Value, Date, Type, Val)};
+                Val = findName(maps:to_list(Monitor), Station),
+                Measurements = maps:get(Val, Monitor),
+                Monitor2 = maps:remove(Val, Monitor),
+                maps:put(Station, addMeasurement(Value, Date, Type, Measurements), Monitor2);
             false -> throw("not defined Station")
             end
         end.
 
+%działa
 removeValue(Station, Date, Type, Monitor) ->
     case maps:is_key(Station, Monitor) of
         true -> 
             Name = Station,
             Val = maps:get(Station, Monitor),
             maps:remove(Station, Monitor),
-            Monitor#{Name => removeMeasurement(Val, Date, Type)};
+            Monitor#{Name => removeMeasurement(Val, Date, Type)}; %poprawić XDDDDDDDDD
         false -> 
-            case findCoords(maps:values(Monitor), Station) of
+            case findCoords(Station, maps:values(Monitor)) of
             true -> 
-                Val = maps:get(Station, Monitor),
-                Monitor2 = maps:remove(Station, Monitor),
-                Monitor2#{Station => removeMeasurement(Val, Date, Type)};
+                Val = findName(maps:to_list(Monitor), Station),
+                Value = maps:get(Val, Monitor),
+                Monitor2 = maps:remove(Val, Monitor),
+                maps:put(Val, removeMeasurement(Value, Date, Type), Monitor2);
             false -> throw("Not defined Station")
         end
     end.
 
+%działa
 getOneValue(Station, Date, Type, Monitor) ->
-    case maps:is_key(Station) of
+    case maps:is_key(Station,Monitor) of
         true -> getMeasurement(Date, Type, maps:get(Station,Monitor));
         false -> throw("Not defined Station")
     end.
 
-getStationMean(_, _, #{}) -> throw("No stations added");
+%działa
 getStationMean(Station, Type, Monitor) ->
-    case maps:is_key(Station) of
+    case maps:is_key(Station, Monitor) of
         true -> getMeanTypeMeasurement(maps:get(Station, Monitor), Type);
         false -> throw("Not defined Station")
     end.
 
-getDailyMean(_, _, #{}) -> throw("No stations added");
-getDailyMean(Type, Day, Monitor) -> getMeanDayMeasurement(Type, Day, maps:values(Monitor), 0, 0).
+getDailyMean(Type, Day, Monitor) -> getMeanDayMeasurement(Type, Day, maps:values(Monitor), 0, 0). %"PM2,5", {{2017,4,24},{11,49,8}}, [{#coord{},[]}|Tail], 0, 0
 
-addMeasurement(Value, Date, Type, {Coords, [List]}) -> 
-    case isDate(Date, [List]) of
-        false   -> {Coords, [List | #measurement{type = Type, value = Value, date = Date}]};
+%działa
+addMeasurement(Value, Date, Type, {Coords, List}) -> 
+    case isDate(Date, List) of
+        false   -> {Coords, lists:merge(List, [#measurement{type = Type, value = Value, date = Date}])};
         _       -> throw("Date exists")
     end.
 
+%działa
 removeMeasurement({Coords, Measurements}, Date, Type) ->
     case isDate(Date, Measurements) of
         true   -> {Coords, deleteMeasurement(Measurements, Date, Type)};
         _       -> throw("Date or Type doesn't exist!")
     end.
 
+%działa
 getMeanDayMeasurement(_, _, [], _, Count) when Count == 0 -> throw("No measures :c");
+getMeanDayMeasurement(_, _, [{_,[]}|[]], _, 0) -> throw("No measures :c");
 getMeanDayMeasurement(_, _, [], Sum, Count) -> Sum/Count;
-getMeanDayMeasurement(Type, Day, {Coords, [#measurement{type = Type1, value = Value, date = Day1}|T]}, Sum, Count) when (Type == Type1 andalso Day == Day1) -> getMeanDayMeasurement(Type, Day, {Coords, T}, Sum + Value, Count + 1);
-getMeanDayMeasurement(Type, Day, {Coords, [#measurement{type = _, value = _, date = _}|T]}, Sum, Count) -> getMeanDayMeasurement(Type, Day, {Coords, T}, Sum, Count).
+getMeanDayMeasurement(_, _, [{_,[]}|[]], Sum, Count) -> Sum/Count;
+getMeanDayMeasurement(Type, {{Y,M,D},_}, [{_,[]}|Next], Sum, Count) -> getMeanDayMeasurement(Type, {{Y,M,D},{}}, Next, Sum, Count);
+getMeanDayMeasurement(Type, {{Y,M,D},_}, [{Coord,[#measurement{type=Type, value = Value, date ={{Y,M,D},_}} | T]} | Next], Sum, Count) -> getMeanDayMeasurement(Type, {{Y,M,D},{}}, lists:merge([{Coord, T}],Next), Sum + Value, Count +1);
+getMeanDayMeasurement(Type, {{Y,M,D},_}, [{Coord,[#measurement{type=_, value = _, date = _} | T]} | Next], Sum, Count) -> getMeanDayMeasurement(Type, {{Y,M,D},{}}, lists:merge([{Coord, T}],Next), Sum, Count).
 
-getMeanTypeMeasurement({_, [H | T]}, Type) ->
-    case isType(Type, [H|T]) of
-        true    -> getMeanTypeMeasurement2([H|T], Type, 0, 0);
-        _       -> throw("No measurements of this type")
-    end.
-
+%działa
+getMeanTypeMeasurement({_, Measurements}, Type) -> getMeanTypeMeasurement2(Measurements, Type, 0, 0).
+%działa
 getMeanTypeMeasurement2([], _, _, Count) when Count == 0 -> throw("No measures :c");
 getMeanTypeMeasurement2([], _, Sum, Count) -> Sum/Count;
-getMeanTypeMeasurement2([#measurement{type = Type1, value = Value, date = _}|T], Type, Sum, Count) when Type =:= Type1 -> getMeanTypeMeasurement2(T, Type, Sum + Value, Count + 1);
+getMeanTypeMeasurement2([#measurement{type = Type, value = Value, date = _}|T], Type, Sum, Count) -> getMeanTypeMeasurement2(T, Type, Sum + Value, Count + 1);
 getMeanTypeMeasurement2([#measurement{type = _, value = _, date = _}|T], Type, Sum, Count) -> getMeanTypeMeasurement2(T, Type, Sum, Count).
 
-findCoords([{SearchFor, _}|_], SearchFor)   -> true;
-findCoords([{_, _}|Tail], SearchFor)        -> findCoords(Tail, SearchFor);
-findCoords([],_)                            -> flase.
+%działa
+findCoords(_,[]) -> false;
+findCoords({X,Y}, [{#coordinates{horizontal = X, vertical = Y},_}|_]) -> true;
+findCoords({X,Y}, [_|List]) -> findCoords({X,Y}, List).
 
+%działa
 deleteMeasurement([#measurement{type = Type, value = _, date = Date}| Tail], Date, Type) -> Tail;
-deleteMeasurement([Head | Tail], Date, Type) -> lists:merge(Head, deleteMeasurement(Tail, Date, Type));
-deleteMeasurement([], _, _) -> throw("Haven't found measurement").
+deleteMeasurement([Head | Tail], Date, Type) -> lists:merge([Head], deleteMeasurement(Tail, Date, Type));
+deleteMeasurement([], _, _) -> "Haven't found measurement".
+
+%działa
+getMeasurement(_, _, {_, []}) -> throw("Haven't found measurement");
+getMeasurement(Date, Type, {_, [#measurement{type = Type, value = Value, date = Date}|_]}) -> Value;
+getMeasurement(Date, Type, {Coords, [#measurement{type = _, value = _, date = _}|T]}) -> getMeasurement(Date, Type, {Coords, T}).
+
+%działa
+isDate(_, []) -> false;
+isDate(Date, [#measurement{type = _, value = _, date = Date}|_]) -> true;
+isDate(Date, [#measurement{type = _, value = _, date = _}|T]) -> isDate(Date, T).
+
+%działa
+findName([],_) -> throw("Unexpected error");
+findName([{Key, {#coordinates{horizontal = X, vertical = Y},_}} | _], {X, Y}) -> Key;
+findName([_|Tail], {X,Y}) -> findName(Tail, {X,Y}).
